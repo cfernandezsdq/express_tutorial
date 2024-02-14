275a719eeb5018c26bb1fa064954f438289aeae4
Carlos Fernandez <carlos.fernandez.sdq@gmail.com>
2024-02-14 09:26:23 -0400

    http-request-object

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.write('<h1>Home Page</h1>')
-    res.end();
+    console.log(req.url);
+    const url = req.url;
+    // home page
+    if(url === '/'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>Home Page</h1>')
+        res.end();
+    }
+    // about page
+    else if(url === '/about'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>About page</h1>')
+        res.end();
+    }
+    // 404 not found
+    else {
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write('<h1>Page not found</h1>')
+        res.end();
+    }
+
 });
 
 server.listen(5000);
